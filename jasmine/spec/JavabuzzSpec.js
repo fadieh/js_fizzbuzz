0a595d55a9144b008c2e333c44d8a8c9d7998422
Fadie Hannona <fadiehannona@gmail.com>
2014-10-14 11:08:51 +0100

    First commit

---
+++ jasmine/spec/JavabuzzSpec.js
@@ -0,0 +1,21 @@
+describe('Javabuzz', function() {
+
+	var javabuzz;
+
+	describe('knows when a number is', function () {
+
+	it('divisible by 3', function() {
+
+		javabuzz = new Javabuzz();
+		expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+
+		});
+
+	it('divisble by 3', function() {
+
+		javabuzz = new Javabuzz();
+		expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+	});
+
+	});
+});
---
+++ jasmine/src/Javabuzz.js
@@ -0,0 +1,5 @@
+function Javabuzz() {};
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+	return (number % 3 === 0);
+};
